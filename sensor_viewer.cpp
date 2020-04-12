163645674adf3e3df91a25a92a08489cc9bfdf96
Timo Sandmann <mail@timosandmann.de>
2020-04-12 16:42:03 +0200

    file documentation added

--- sensor_viewer.cpp
+++ sensor_viewer.cpp
@@ -1,3 +1,27 @@
+/*
+ * This file is part of the c't-Bot remote viewer tool.
+ * Copyright (c) 2020 Timo Sandmann
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @file    sensor_viewer.cpp
+ * @brief   Sensor viewer component
+ * @author  Timo Sandmann
+ * @date    11.04.2020
+ */
+
 #include <QString>
 #include <QQmlContext>
 #include <QDebug>
